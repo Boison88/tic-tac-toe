6d5fe7ce298e08d5c9e45f3433ed5a590ec646d0
boison <boison88@mail.ru>
2023-07-12 23:15:08 +0300

    0.4 added text to the display

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,20 @@
 import pygame
 pygame.init()
 
-size = (600, 400)
-pygame.display.set_mode(size)
+DISPLAY_SIZE = (800, 800)
+WHITE_RGB = (255, 255, 255)
+BLACK_RGB = (0, 0, 0)
+
+screen = pygame.display.set_mode(DISPLAY_SIZE)
 pygame.display.set_caption("tic-tac-toe")
 img = pygame.image.load("image_tictactoe.jpg")
 pygame.display.set_icon(img)
+font = pygame.font.SysFont('futura', 32)
+welcome_text = font.render("Let's play!", 1, WHITE_RGB, BLACK_RGB)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+    screen.blit(welcome_text, (320, 60))
+    pygame.display.update()
